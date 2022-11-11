58a7303e990d130d69ce47e6a6c2d48a7208ff01
Jack Paget <jackpaget1@gmail.com>
2022-11-11 21:52:00 +0000

    Styled searchbar

--- components/home/Searchbar.jsx
+++ components/home/Searchbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Search } from 'react-bootstrap-icons';
 import axios from 'axios';
 
 const API_KEY = 'AIzaSyADAV_RuIVe-nZHJFf2HxmKdDHtvE_zAmM';
@@ -14,26 +15,31 @@ const Searchbar = ({ setBookData }) => {
   };
 
   const fetchSearchData = () => {
-    axios
-      .get(
-        `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=5&key=${API_KEY}`
-      )
-      .then((res) => {
-        const data = res.data.items;
-        setBookData(data);
-      });
+    if (search.length > 0) {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=5&key=${API_KEY}`
+        )
+        .then((res) => {
+          const data = res.data.items;
+          setBookData(data);
+        });
+    }
   };
 
   return (
-    <>
+    <div className='bg-charcoal rounded-lg flex items-center w-1/2'>
       <input
         onKeyDown={(e) => handleKeyDown(e)}
         onChange={(e) => setSearch(e.target.value)}
         type='text'
         placeholder='Search'
-        className='bg-charcoal placeholder-white/20 text-neutral-300 pl-4 py-3 w-1/2 rounded-lg font-inter outline-none'
+        className='bg-charcoal placeholder-white/20 text-neutral-300 pl-4 py-3 w-full rounded-lg font-inter outline-none focus:ring-1 ring-white/50'
       />
-    </>
+      <button className='px-3' onClick={fetchSearchData}>
+        <Search color='white' size={24} />
+      </button>
+    </div>
   );
 };
 
