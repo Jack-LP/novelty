dc9926bef99cf00e9b693cf2fedf6f9ce894c2a7
Jack Paget <jackpaget1@gmail.com>
2022-11-12 00:32:18 +0000

    Created title shuffle animation

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -1,12 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Searchbar from './Searchbar';
+import { heroBookTitles } from '../../data/heroBookTitles';
+import TypewriterComponent from 'typewriter-effect';
 
 const Hero = ({ setBookData }) => {
+  useEffect(() => {
+    const array = heroBookTitles;
+    const shuffleTitles = () => {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+    };
+    shuffleTitles();
+  }, []);
+
   return (
     <div className='flex flex-col gap-14'>
       <div className='text-6xl text-white font-playfair font-bold'>
         <h1>How long does it take to read...</h1>
-        <span>The Hobbit?</span>
+        <TypewriterComponent
+          options={{
+            strings: heroBookTitles,
+            autoStart: true,
+            loop: true,
+          }}
+        />
       </div>
       <Searchbar setBookData={setBookData} />
     </div>
