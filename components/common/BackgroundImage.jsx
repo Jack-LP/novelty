2702bed09288d950873efc6ef91f2f707594fc59
Jack Paget <jackpaget1@gmail.com>
2022-11-22 21:02:24 +0000

    Fixed background image bug

--- components/common/BackgroundImage.jsx
+++ components/common/BackgroundImage.jsx
@@ -5,7 +5,7 @@ const BackgroundImage = ({ image }) => {
     <>
       <div className='w-screen h-screen absolute top-0 left-0 bg-gradient-to-t from-black/50 to-transparent -z-10' />
       <img
-        className='absolute top-0 left-0 w-screen h-screen object-cover -z-20 pointer-events-none'
+        className='fixed top-0 left-0 w-screen h-screen object-cover -z-20 pointer-events-none'
         src={image}
         alt='background image'
       />
